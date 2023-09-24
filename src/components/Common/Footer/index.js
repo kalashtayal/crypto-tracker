cdbad70a2ade59829a40dca379ca0fb0e75ed1eb
tayal <kalashtayal2000@gmail.com>
2023-09-24 14:12:20 +0530

    crypto

--- src/components/Common/Footer/index.js
+++ src/components/Common/Footer/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './style.css'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function Footer() {
   return (
-    <div>
-    <h2>crypto tracker . </h2>
+    <div className='footer'>
+    <h2>crypto tracker . 
+   
+    </h2>
+
+    <span>
+    <a href='https://www.instagram.com/'> <InstagramIcon/></a>
+    <a href='https://github.com/kalashtayal'> <GitHubIcon/></a>
+    <a href='https://www.linkedin.com/feed/?trk=404_page'> <LinkedInIcon/></a>
+      
+    </span>
     </div>
   )
 }
